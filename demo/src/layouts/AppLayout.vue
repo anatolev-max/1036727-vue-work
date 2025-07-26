28577fd65d64a6ae20ae89d683690f8fa6c0043d
Todd <anatolev.web@icloud.com>
2025-07-26 22:48:18 +1000

    task-1.20 - layouts

---
+++ demo/src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="app_layout">
+        <app-layout-header/>
+        <slot/>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
