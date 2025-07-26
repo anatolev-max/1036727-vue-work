93a35584692171b01f30f1cf7d9f42663ef8a879
Todd <anatolev.web@icloud.com>
2025-07-26 23:53:15 +1000

    task-1.31 - add logic to counter

--- demo/src/demo0/DemoCounter.vue
+++ demo/src/demo0/DemoCounter.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
-        <button>-</button>
-        <span>Current value: 0</span>
-        <button>+</button>
+        <button @click="decrement">-</button>
+        <span>Current value: {{ count }}</span>
+        <button @click="increment">+</button>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+import {ref} from 'vue'
+
+const count = ref(0);
+
+const decrement = () => {
+    count.value = count.value - 1;
+}
+
+const increment = () => {
+    count.value = count.value + 1;
+}
 </script>
 
-<style scoped lang="scss">
+<style>
+    body {
+        padding: 20px;
+    }
+</style>
+
+<style scoped>
+button {
+    padding: 5px 10px;
+}
+
+span {
+    margin: 0 10px;
+}
 </style>
