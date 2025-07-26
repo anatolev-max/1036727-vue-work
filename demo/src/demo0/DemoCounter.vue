33cdacae416200e53920084945b554e629808c9f
Todd <anatolev.web@icloud.com>
2025-07-26 22:32:38 +1000

    task-1.19 - create template for counter vue-component

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button>-</button>
+        <span>Current value: 0</span>
+        <button>+</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
