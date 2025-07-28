4e1a8e90f1fdaf9e058f695c8382247f1dcf590b
maksim.anatolev <maksim.anatolev@fstravel.com>
2025-07-28 23:46:36 +0300

    2.4 - app-icon

---
+++ frontend/src/common/components/AppIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <button
+        class="column__button column__update icon"
+        type="button"
+    />
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/components/app-icon.scss";
+</style>
